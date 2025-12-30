35ac1fd1570b8f3a3496ca6cb7d973fab2162f1f
ShubhamSingh <shubh.singh181296@gmail.com>
2025-12-30 17:54:52 +0530

    Fetch Types

--- src/main/java/com/ecommerce/productservicejuly2025/models/Category.java
+++ src/main/java/com/ecommerce/productservicejuly2025/models/Category.java
@@ -2,6 +2,7 @@ package com.ecommerce.productservicejuly2025.models;
 
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
--- src/test/java/com/ecommerce/productservicejuly2025/ProductServiceJuly2025ApplicationTests.java
+++ src/test/java/com/ecommerce/productservicejuly2025/ProductServiceJuly2025ApplicationTests.java
@@ -1,7 +1,11 @@
 package com.ecommerce.productservicejuly2025;
 
+import com.ecommerce.productservicejuly2025.models.Category;
+import com.ecommerce.productservicejuly2025.models.Product;
 import com.ecommerce.productservicejuly2025.projections.ProductWithIdAndTitle;
+import com.ecommerce.productservicejuly2025.repositories.CategoryRepository;
 import com.ecommerce.productservicejuly2025.repositories.ProductRepository;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,6 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.net.SocketOption;
 import java.sql.SQLOutput;
 import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 class ProductServiceJuly2025ApplicationTests {
@@ -16,29 +21,41 @@ class ProductServiceJuly2025ApplicationTests {
     @Autowired
     private ProductRepository productRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Test
     void contextLoads() {
     }
 
     @Test
+    @Transactional
     void testDbQueries()
     {
-        List<ProductWithIdAndTitle> productWithIdAndTitles=
-                productRepository.randomSearchMethodForProduct();
+//        List<ProductWithIdAndTitle> productWithIdAndTitles=
+//                productRepository.randomSearchMethodForProduct();
+//
+//        for(ProductWithIdAndTitle product:productWithIdAndTitles)
+//        {
+//            System.out.println(product.getId()+" "+product.getTitle());
+//        }
+//
+//        System.out.println("DEBUG");
+//
+//        List<ProductWithIdAndTitle> productWithIdAndTitlesFromNative=
+//                productRepository.nativeSearchMethodForProduct();
+//
+//        for(ProductWithIdAndTitle product:productWithIdAndTitlesFromNative)
+//        {
+//            System.out.println(product.getId()+" "+product.getTitle());
+//        }
 
-        for(ProductWithIdAndTitle product:productWithIdAndTitles)
-        {
-            System.out.println(product.getId()+" "+product.getTitle());
-        }
+        Optional<Product> product=productRepository.findById(9L);
 
-        System.out.println("DEBUG");
+        Optional<Category> category=categoryRepository.findById(1L);
 
-        List<ProductWithIdAndTitle> productWithIdAndTitlesFromNative=
-                productRepository.nativeSearchMethodForProduct();
-
-        for(ProductWithIdAndTitle product:productWithIdAndTitlesFromNative)
-        {
-            System.out.println(product.getId()+" "+product.getTitle());
+        if(category.isPresent()) {
+            System.out.println("product:" + category.get().getProducts());
         }
 
         System.out.println("DEBUG");
